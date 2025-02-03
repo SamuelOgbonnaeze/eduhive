83e6e939c5971863480a3f0e6b295c0efe0684ee
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2025-02-03 12:06:51 +0100

    color mode bug fix

--- app/(student)/students/_components/student-grade.tsx
+++ app/(student)/students/_components/student-grade.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { ProgressCircleRing, ProgressCircleRoot, ProgressCircleValueText } from "@/components/ui/progress-circle";
-import { BookOpenText, ClipboardList, Clock3, GraduationCap } from 'lucide-react';
+import { BookOpenText, ClipboardList, Clock3, } from 'lucide-react';
 
 interface GradeCardProps {
     progressValue: number;
@@ -9,44 +9,42 @@ interface GradeCardProps {
     lessonNumber: number;
     duration: number;
     assignmentNumber: number;
-    studentNumber: number;
 
 }
-const GradeCard = ({ progressValue, subject, lessonNumber, duration, assignmentNumber, studentNumber }: GradeCardProps) => {
+const GradeCard = ({ progressValue, subject, lessonNumber, duration, assignmentNumber }: GradeCardProps) => {
     return (
-        <div className="max-w-[420px]  bg-[#F0F8FF] border border-[#0F52BA33] rounded-[10px] ">
-            <div className="w-full flex p-3 sm:p-4 gap-x-3 sm:gap-x-4 pb-[40px] md:pb-[50px]">
-                <div>
-                    <ProgressCircleRoot colorPalette={"blue"} value={progressValue} size={"lg"}>
-                        <ProgressCircleValueText />
-                        <ProgressCircleRing />
-                    </ProgressCircleRoot>
-                </div>
-                <div className="flex flex-col">
-                    <p className="font-medium text-lg sm:text-xl">{subject}</p>
-                    <div className="mt-2 grid grid-cols-2 gap-x-4 gap-y-4 sm:gap-x-5 sm:gap-y-5">
-                        <div className="flex items-center gap-x-1">
-                            <BookOpenText className="text-[#0F52BA]" size={18} />
-                            <p className="text-[#292929] font-normal text-xs sm:text-sm">{lessonNumber} Lesson</p>
-                        </div>
-
-                        <div className="flex items-center gap-x-1">
-                            <Clock3 className="text-[#0F52BA]" size={18} />
-                            <p className="text-[#292929] font-normal text-xs sm:text-sm">{duration} mins</p>
-                        </div>
-
-                        <div className="flex items-center gap-x-1">
-                            <ClipboardList className="text-[#0F52BA]" size={18} />
-                            <p className="text-[#292929] font-normal text-xs sm:text-sm">{assignmentNumber} assignment</p>
-                        </div>
-
-                        <div className="flex items-center gap-x-1">
-                            <GraduationCap className="text-[#0F52BA]" size={18} />
-                            <p className="text-[#292929] font-normal text-xs sm:text-sm">{studentNumber} students</p>
-                        </div>
+        <div className="max-w-[320px]  bg-[#F0F8FF] border border-[#0F52BA33] rounded-[10px] ">
+            <div className=" p-1 sm:p-2 pb-[40px] md:pb-[50px]">
+                <div className="w-full flex items-center  gap-x-3 sm:gap-x-4 ">
+                    <div>
+                        <ProgressCircleRoot colorPalette={"blue"} value={progressValue} size={"lg"}>
+                            <ProgressCircleValueText />
+                            <ProgressCircleRing />
+                        </ProgressCircleRoot>
+                    </div>
+                    <div className="">
+                        <p className="font-medium text-lg sm:text-xl">{subject}</p>
                     </div>
                 </div>
+                <div className="w-full mt-2 grid grid-cols-2 gap-x-4 gap-y-4 sm:gap-x-5 sm:gap-y-5">
+                    <div className="flex items-center gap-x-1">
+                        <BookOpenText className="text-[#0F52BA]" size={18} />
+                        <p className="text-[#292929] font-normal text-xs sm:text-sm">{lessonNumber} Lesson</p>
+                    </div>
+
+                    <div className="flex items-center gap-x-1">
+                        <Clock3 className="text-[#0F52BA]" size={18} />
+                        <p className="text-[#292929] font-normal text-xs sm:text-sm">{duration} mins</p>
+                    </div>
+
+                    <div className="flex items-center gap-x-1">
+                        <ClipboardList className="text-[#0F52BA]" size={18} />
+                        <p className="text-[#292929] font-normal text-xs sm:text-sm">{assignmentNumber} assignment</p>
+                    </div>
+
+                </div>
             </div>
+
         </div>
     )
 }
@@ -62,7 +60,7 @@ const StudentGrade = () => {
                 </Link>
             </div>
 
-            <div className="mt-[20px] grid grid-cols-1 lg:grid-cols-2 items-center justify-between gap-y-3">
+            <div className="mt-[20px] grid grid-cols-1 lg:grid-cols-3 items-center justify-between gap-y-3">
 
                 <GradeCard
                     progressValue={79}
@@ -70,17 +68,25 @@ const StudentGrade = () => {
                     lessonNumber={21}
                     duration={52}
                     assignmentNumber={6}
-                    studentNumber={420}
                 />
 
 
                 <GradeCard
-                    progressValue={60}
+                    progressValue={80}
                     subject="Organic Chemistry"
                     lessonNumber={10}
                     duration={45}
                     assignmentNumber={4}
-                    studentNumber={412}
+
+                />
+
+                <GradeCard
+                    progressValue={52}
+                    subject="Mathematics"
+                    lessonNumber={10}
+                    duration={45}
+                    assignmentNumber={4}
+
                 />
             </div>
         </div>
